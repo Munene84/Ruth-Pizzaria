aa7ed4844e0aa176e7af17a3f60b78aefecb436c
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-29 17:16:32 +0300

    added the pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ var newTotal;
 var tp1="",tp2="",tp3="",tp4="";
 var delivery;
 var orderLocation
+
+/business logic
+
+//pizza constructor
+function Pizza(size,crust,number,delivery,orderLocation,tp1,tp2,tp3,tp4){
+    this.size=size;
+    this.crust=crust;
+    this.number=number;
+    this.delivery=delivery;
+    this.orderLocation=orderLocation;
+    this.tp1=tp1;
+    this.tp2=tp2;
+    this.tp3=tp3;
+    this.tp4=tp4;
+}
